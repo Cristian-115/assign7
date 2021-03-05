ef24b07f0fcd3295d4db26ed827c7ae8ebd3791e
Cristian Garibay <garibcri@oregonstate.edu>
2021-03-04 16:27:43 -0800

    wrote test that fails

--- tests_fuzzbuzz.py
+++ tests_fuzzbuzz.py
@@ -3,5 +3,10 @@ import fuzzbuzz
 
 class TestCase(unittest.TestCase):
     def test_Fizz(self):
-        self.assertEqual(fuzzbuzz.Fuzzbuzz(), 1)
+        self.assertEqual(fuzzbuzz.Fuzzbuzz(5), "Buzz")
         
+
+
+
+if __name__ == '__main__':
+    unittest.main()
